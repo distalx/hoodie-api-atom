7c8b3b2bc02b971e8f02c2c0f7ed92b3782767c0
distalx <distalx@gmail.com>
2016-12-28 22:07:21 +0530

    this is where it all begins

---
+++ README.md
@@ -0,0 +1,3 @@
+# hoodie-api package
+
+ A package for hoodie for atom editor with autocomplete, code snippets, color-coded grammar, syntax highlighting, and more! Code faster and with fewer mistakes!
---
+++ spec/hoodie-api-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HoodieApiView from '../lib/hoodie-api-view';
+
+describe('HoodieApiView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
